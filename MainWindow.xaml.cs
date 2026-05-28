47ddb6d7c91dc72c0ce3779c99f0863daf1f72a8
Xichavo Shaun <xichavo205@gmail.com>
2026-05-28 16:41:13 +0200

    Translated the logo and audio to GUI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,15 +14,28 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace cybersecurity_chatbot_p2
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{//start of namespace
     public partial class MainWindow : Window
-    {
+    {//start of class
         public MainWindow()
-        {
+        {//start of constructor
             InitializeComponent();
-        }
-    }
-}
+
+            //creating an instance of the voice_greeting class without an object name
+            new voice_greeting();
+
+        }//end of constructor
+
+        private void proceed(object sender, RoutedEventArgs e)
+        {//start of method
+
+        }//end of method
+
+        private void submit_name(object sender, RoutedEventArgs e)
+        {//start of method
+
+        }//end of method
+
+    }//end of class
+
+}//end of namespace
---
+++ voice_greeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Media;
+
+namespace cybersecurity_chatbot_p2
+{//start of namespace
+    internal class voice_greeting
+    {//start of class
+        public voice_greeting()
+        {//start of constructor
+
+            //replace the \bin\Debug\ from the path with greeting.wav
+
+            string auto_path = AppDomain.CurrentDomain.BaseDirectory.Replace(@"\bin\Debug\", @"\greet.wav");
+
+            //create an instance for the soundPlayer class
+            SoundPlayer greetMe = new SoundPlayer(auto_path);
+            //then greet
+            greetMe.Play();
+
+        }//end of constructor
+
+    }//end of class
+
+}//end of namespacee
